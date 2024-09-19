eef3a374aa2f26d5dca97361b6593b74523c0d07
FaisalNadaf <faisalfnadaf@gmail.com>
2024-09-19 21:56:27 +0530

    code

--- lib/pages/feedPage.dart
+++ lib/pages/feedPage.dart
@@ -1,17 +1,71 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:finstagram/services/firebase_service.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_social_media/services/firebase_dervices.dart';
+import 'package:get_it/get_it.dart';
 
 class FeedPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return _feedPageState();
+    return _FeedPageState();
   }
 }
 
-class _feedPageState extends State<FeedPage> {
+class _FeedPageState extends State<FeedPage> {
+  double? _deviceHeight, _deviceWidth;
+  FireBaseService? _firebaseService;
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseService = GetIt.instance.get<FireBaseService>();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Container(
+      height: _deviceHeight!,
+      width: _deviceWidth!,
+      child: _postsListView(),
+    );
+  }
+
+  Widget _postsListView() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firebaseService!.getLatestPosts(),
+      builder: (BuildContext _context, AsyncSnapshot _snapshot) {
+        if (_snapshot.hasData) {
+          List _posts = _snapshot.data!.docs.map((e) => e.data()).toList();
+          return ListView.builder(
+            itemCount: _posts.length,
+            itemBuilder: (BuildContext context, int index) {
+              Map _post = _posts[index];
+              return Container(
+                height: _deviceHeight! * 0.30,
+                margin: EdgeInsets.symmetric(
+                  vertical: _deviceHeight! * 0.01,
+                  horizontal: _deviceWidth! * 0.05,
+                ),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                      _post["image"],
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
